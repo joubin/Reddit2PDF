3ebd21c58b3bae4930e8891cace3ceb7a87a0beb
Joubin Jabbari <joubin.j@gmail.com>
2017-01-10 22:28:00 -0800

    commiting automaticall with a script before restore of system

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-sudo apt-get install python-devel
-sudo apt-get install python-setuptools
+installer=''
+if ["$(uname)" == "Darwin" ]; then
+    installer="brew"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    installer="sudo apt-get"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    exit(1);
+fi
+$installer install python-devel
+$installer  install python-setuptools
 sudo easy_install pdfkit
-apt-get install gcc -y
-easy_install pip
-STATIC_DEPS=true pip install readability-lxml
-STATIC_DEPS=true pip install tldextract
-STATIC_DEPS=true pip install imgurpython
+$installer install gcc -y
+sudo easy_install pip
+sudo STATIC_DEPS=true pip install readability-lxml
+sudo STATIC_DEPS=true pip install tldextract
+sudo STATIC_DEPS=true pip install imgurpython
